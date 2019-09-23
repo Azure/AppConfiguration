f39892e227f440934f116eee733d1672fff524b6
Vijay Sankar Ganesh <visanka@microsoft.com>
2019-09-23 09:38:59 -0700

    Added builder.Build() for null check

--- examples/DotNetCore/WebJobs/WebJobHelloWorld/WebJobHelloWorld/Program.cs
+++ examples/DotNetCore/WebJobs/WebJobHelloWorld/WebJobHelloWorld/Program.cs
@@ -37,7 +37,6 @@ namespace WebJobHelloWorld
             //
             // This application attempts to connect to Azure App Configuration to retrieve Azure Blob Storage name, and Queue Name for the Azure Web Job.
             // It attempts to read these value from appsettings.json, environment variables and Azure App Configuration (in that order, with newer value overriding older ones).
-            var configBuilder = new ConfigurationBuilder();
             configBuilder.AddJsonFile("appsettings.json")
                 .AddEnvironmentVariables();
 
@@ -51,6 +50,8 @@ namespace WebJobHelloWorld
                         .TrimKeyPrefix("WebJob:");
                 });
             }
+
+            config = configBuilder.Build();
 
             if (string.IsNullOrEmpty(config["AzureWebJobsStorage"]))
             {
