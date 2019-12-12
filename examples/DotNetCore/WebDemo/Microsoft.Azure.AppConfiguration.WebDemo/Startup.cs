73052f6f0fdd6d755254000814d97f1f994b4e14
Zhenlan Wang <zhenlwa@microsoft.com>
2019-12-12 11:27:58 -0800

    Update link to error page

--- examples/DotNetCore/WebDemo/Microsoft.Azure.AppConfiguration.WebDemo/Startup.cs
+++ examples/DotNetCore/WebDemo/Microsoft.Azure.AppConfiguration.WebDemo/Startup.cs
@@ -33,7 +33,7 @@ namespace Microsoft.Azure.AppConfiguration.WebDemo
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
