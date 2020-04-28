3cf9fc1171fbaa2ed9964b79e7303850aecafbc2
Avani Gupta <avanigupta@users.noreply.github.com>
2020-04-28 10:51:40 -0700

    KeyLabel cast using pattern matching

--- examples/ConfigurationStoreBackup/KeyLabel.cs
+++ examples/ConfigurationStoreBackup/KeyLabel.cs
@@ -15,16 +15,12 @@ namespace ConfigurationStoreBackup
 
         public override bool Equals(Object obj)
         {
-            if (obj is KeyLabel)
+            if (obj is KeyLabel keyLabel)
             {
-                KeyLabel keyLabel = (KeyLabel)obj;
                 return string.Equals(this.Key, keyLabel.Key, StringComparison.Ordinal)
                     && string.Equals(this.Label, keyLabel.Label, StringComparison.Ordinal);
             }
-            else
-            {
-                return false;
-            }
+            return false;
         }
 
         public override int GetHashCode()
