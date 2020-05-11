15b68057353001ca8d6ad9eb8d003755db79a538
Vijay Sankar Ganesh <visanka@microsoft.com>
2020-05-11 12:35:50 -0700

    Copied existing WebDemo app to be changed to use EventHub for refresh

---
+++ examples/DotNetCore/WebDemoWithEventHub/WebDemoWithEventHub/Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿namespace WebDemoWithEventHub.Pages
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Options;
+
+    public class IndexModel : PageModel
+    {
+        public IndexModel(IOptionsSnapshot<Settings> options)
+        {
+            Settings = options.Value;
+        }
+
+        public Settings Settings { get; }
+    }
+}
