d44e49a6532a73c41c8708d98b30077ce17d4c8f
Zhenlan Wang <zhenlwa@microsoft.com>
2021-03-25 14:15:54 -0700

    Add a queue triggered sample function (#492)
    Add a queue triggered sample function with queue name defined in Azure App Configuration.

--- examples/DotNetCore/AzureFunction/FunctionApp/ShowMessage.cs
+++ examples/DotNetCore/AzureFunction/FunctionApp/ShowMessage.cs
@@ -7,7 +7,6 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace FunctionApp
 {
