f58e2162e896a9011f6bede6427845b9c953cee2
Zhenlan Wang <zhenlwa@microsoft.com>
2021-10-25 19:29:40 -0700

    Add feature flags to the ASP.NET Web Forms example (#565)
    Add feature flags to the ASP.NET Web Forms example

--- examples/DotNetFramework/WebFormApp/WebFormApp/Global.asax.cs
+++ examples/DotNetFramework/WebFormApp/WebFormApp/Global.asax.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
 
 namespace WebFormApp
 {
     public class Global : System.Web.HttpApplication
     {
         public static IConfiguration Configuration;
+        public static IFeatureManager FeatureManager;
 
         private static IConfigurationRefresher _configurationRefresher;
+        private static IServiceProvider _serviceProvider;
 
         protected void Application_Start(object sender, EventArgs e)
         {
@@ -17,18 +21,30 @@ namespace WebFormApp
             builder.AddAzureAppConfiguration(options =>
             {
                 options.Connect(Environment.GetEnvironmentVariable("ConnectionString"))
-                       // Load all keys that start with `TestApp:`
-                       .Select("TestApp:*")
+                       // Load all keys that start with `TestApp:` and have no label
+                       .Select("TestApp:*", LabelFilter.Null)
                        // Configure to reload configuration if the registered key 'TestApp:Settings:Sentinel' is modified.
-                       // Use the default cache expiration time of 30 seconds that can be changed by calling SetCacheExpiration.
-                       .ConfigureRefresh(refresh => 
+                       // Use the default cache expiration of 30 seconds. It can be overriden via AzureAppConfigurationOptions.SetCacheExpiration.
+                       .ConfigureRefresh(refresh =>
                        {
-                           refresh.Register("TestApp:Settings:Sentinel", refreshAll:true);
-                       });
+                           refresh.Register("TestApp:Settings:Sentinel", refreshAll: true);
+                       })
+                       // Load all feature flags with no label. To load specific feature flags and labels, set via FeatureFlagOptions.Select.
+                       // Use the default cache expiration of 30 seconds. It can be overriden via FeatureFlagOptions.CacheExpirationInterval.
+                       .UseFeatureFlags();
+
                 _configurationRefresher = options.GetRefresher();
             });
 
+            // Build configuration
             Configuration = builder.Build();
+
+            // Build feature manager
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(Configuration)
+                    .AddFeatureManagement();
+            _serviceProvider = services.BuildServiceProvider();
+            FeatureManager = _serviceProvider.GetRequiredService<IFeatureManager>();
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
@@ -37,6 +53,11 @@ namespace WebFormApp
             // This will be a no-op if the cache expiration time window is not reached.
             // The configuration is refreshed asynchronously without blocking the execution of the current request.
             _ = _configurationRefresher.TryRefreshAsync();
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            (_serviceProvider as IDisposable).Dispose();
         }
     }
 }
