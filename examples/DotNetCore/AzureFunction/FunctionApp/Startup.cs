a75890b64b7215d2521212742ec57504faf66a03
Zhenlan Wang <zhenlwa@microsoft.com>
2021-11-12 20:01:31 -0800

    Add an example of Azure Functions using isolated mode (#570)
    Add an example of Azure Functions using the isolated mode

--- examples/DotNetCore/AzureFunction/FunctionApp/Startup.cs
+++ examples/DotNetCore/AzureFunction/FunctionApp/Startup.cs
@@ -15,13 +15,15 @@ namespace FunctionApp
             builder.ConfigurationBuilder.AddAzureAppConfiguration(options =>
             {
                 options.Connect(Environment.GetEnvironmentVariable("ConnectionString"))
-                       // Load all keys that start with `TestApp:`
+                       // Load all keys that start with `TestApp:` and have no label
                        .Select("TestApp:*")
-                       // Configure to reload configuration if the registered 'Sentinel' key is modified
+                       // Configure to reload configuration if the registered key 'TestApp:Settings:Sentinel' is modified.
+                       // Use the default cache expiration of 30 seconds. It can be overriden via AzureAppConfigurationOptions.SetCacheExpiration.
                        .ConfigureRefresh(refreshOptions =>
                             refreshOptions.Register("TestApp:Settings:Sentinel", refreshAll: true)
                         )
-                       // Indicate to load feature flags
+                       // Load all feature flags with no label. To load specific feature flags and labels, set via FeatureFlagOptions.Select.
+                       // Use the default cache expiration of 30 seconds. It can be overriden via FeatureFlagOptions.CacheExpirationInterval.
                        .UseFeatureFlags();
             });
         }
---
+++ examples/DotNetCore/AzureFunction/FunctionAppIsolatedMode/AzureAppConfigurationRefreshExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+
+namespace FunctionAppIsolatedMode
+{
+    internal static class AzureAppConfigurationRefreshExtensions
+    {
+        public static IFunctionsWorkerApplicationBuilder UseAzureAppConfiguration(this IFunctionsWorkerApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AzureAppConfigurationRefreshMiddleware>();
+        }
+    }
+}
