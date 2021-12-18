cd2b11acd464dfc7e3df46cc6e333e349c493913
Zhenlan Wang <zhenlwa@microsoft.com>
2021-12-17 16:17:10 -0800

    Add a .NET 6 web app example

---
+++ examples/DotNetCore/WebDemoNet6/WebDemoNet6/Pages/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+
+namespace WebDemoNet6.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Settings Settings { get; }
+
+        public IndexModel(IOptionsSnapshot<Settings> options)
+        {
+            Settings = options.Value;
+        }
+    }
+}
---
+++ examples/DotNetCore/WebDemoNet6/WebDemoNet6/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace WebDemoNet6
+{
+    public class Settings
+    {
+        public string? BackgroundColor { get; set; }
+
+        public long FontSize { get; set; }
+
+        public string? Messages { get; set; }
+    }
+}
