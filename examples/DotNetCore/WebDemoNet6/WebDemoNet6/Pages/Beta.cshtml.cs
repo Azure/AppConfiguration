b8e16678e134d555f9ab1f02766cb0dbaabaa446
Zhenlan Wang <zhenlwa@microsoft.com>
2022-04-05 13:17:08 -0700

    Use FeatureGate support for razor page model

--- examples/DotNetCore/WebDemoNet6/WebDemoNet6/Pages/Beta.cshtml.cs
+++ examples/DotNetCore/WebDemoNet6/WebDemoNet6/Pages/Beta.cshtml.cs
@@ -1,24 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
 
 namespace WebDemoNet6.Pages
 {
+    [FeatureGate("Beta")]
     public class BetaModel : PageModel
     {
-        private IFeatureManagerSnapshot _featureManager;
-
-        public BetaModel(IFeatureManagerSnapshot featureManager)
+        public void OnGet()
         {
-            _featureManager = featureManager;
-        }
-        
-        public async Task<IActionResult> OnGetAsync()
-        {
-            // Respond 404 NotFound if the 'Beta' feature is not enabled
-            return await _featureManager.IsEnabledAsync("Beta")
-                ? Page()
-                : base.NotFound();
         }
     }
 }
