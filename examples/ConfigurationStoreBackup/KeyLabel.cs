45b4d97c5a2522837cfbff5bb08a93a5ae64d8c5
Callon Campbell <calloncampbell@msn.com>
2023-03-07 09:40:48 -0500

    Added back the override

--- examples/ConfigurationStoreBackup/KeyLabel.cs
+++ examples/ConfigurationStoreBackup/KeyLabel.cs
@@ -13,6 +13,16 @@ namespace ConfigurationStoreBackup
             Label = label ?? string.Empty;
         }
 
+        public override bool Equals(Object obj)
+        {
+            if (obj is KeyLabel keyLabel)
+            {
+                return string.Equals(this.Key, keyLabel.Key, StringComparison.Ordinal)
+                    && string.Equals(this.Label, keyLabel.Label, StringComparison.Ordinal);
+            }
+            return false;
+        }
+
         public override int GetHashCode()
         {
             return this.Label != null ? this.Key.GetHashCode() ^ this.Label.GetHashCode() : this.Key.GetHashCode();
