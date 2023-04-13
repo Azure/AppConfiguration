d48ba578d4a5f11d49def0db26fbad514a974215
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2023-04-13 16:27:08 -0500

    revisions, display code in progress

--- examples/DotNetCore/ConsoleAppWithBlobStorageReferences/Program.cs
+++ examples/DotNetCore/ConsoleAppWithBlobStorageReferences/Program.cs
@@ -1,17 +1,16 @@
 ﻿using Azure.Data.AppConfiguration;
 using Azure.Identity;
 using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using System;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Examples.ConsoleApplication
 {
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Configuration.AzureAppConfiguration;
-    using Newtonsoft.Json;
-    using System;
-    using System.Text;
-    using System.Threading;
-    using System.Threading.Tasks;
-
     /*
         Example Blob Content
         -------------------------
@@ -49,74 +48,60 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Examples.Cons
             cts.Cancel();
         }
 
-        private static async Task<MyBlobContent?> ReadBlobContentAsync(Uri blobUri)
+        private static async Task<string> ReadBlobContentAsync(Uri blobUri)
         {
             var blobClient = new BlobClient(blobUri, new DefaultAzureCredential());
 
             // Read blob content
-            var contentStream = new MemoryStream();
-            await blobClient.DownloadToAsync(contentStream);
+            using (var contentStream = new MemoryStream())
+            {
+                await blobClient.DownloadToAsync(contentStream);
 
-            // Go to the beginning of the content stream
-            contentStream.Position = 0;
+                // Go to the beginning of the content stream
+                contentStream.Position = 0;
 
-            // Convert content to json format
-            var reader = new StreamReader(contentStream);
-            var jsonReader = new JsonTextReader(reader);
-
-            var serializer = new JsonSerializer();
-
-            // Save the string values in the JSON array "Data" to MyBlobContent.Data
-            return serializer.Deserialize<MyBlobContent>(jsonReader);
+                // Convert content to string
+                using (var reader = new StreamReader(contentStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
         }
 
         private static void Configure()
         {
             var builder = new ConfigurationBuilder();
 
-            // Load a subset of the application's configuration from a json file and environment variables
-            builder.AddJsonFile("appsettings.json")
-                   .AddEnvironmentVariables();
-
             IConfiguration configuration = builder.Build();
 
-            if (string.IsNullOrEmpty(configuration["ConnectionString"]))
-            {
-                Console.WriteLine("Connection string not found.");
-                Console.WriteLine("Please set the 'ConnectionString' environment variable to a valid Azure App Configuration connection string and re-run this example.");
-                return;
-            }
+            //if (string.IsNullOrEmpty(configuration["ConnectionString"]))
+            //{
+            //    Console.WriteLine("Connection string not found.");
+            //    Console.WriteLine("Please set the 'ConnectionString' environment variable to a valid Azure App Configuration connection string and re-run this example.");
+            //    return;
+            //}
 
             // Augment the configuration builder with Azure App Configuration
             // Pull the connection string from an environment variable
             builder.AddAzureAppConfiguration(options =>
             {
-                options.Connect(configuration["ConnectionString"])
-                        .Select("*")
+                options.Connect(new Uri("https://ajusupovic-ac2.azconfig.io"), new DefaultAzureCredential())
+                        // key: BlobUri
+                        // value: https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}
+                        .Select("BlobUri")
                         .ConfigureRefresh(refresh =>
                         {
-                            // key: BlobUri
-                            // value: https://{account_name}.blob.core.windows.net/{container_name}/{blob_name}
-                            refresh.Register("BlobUri")
-                                    .SetCacheExpiration(TimeSpan.FromSeconds(10));
+                            // Changes to BlobSentinel will refresh all key-values, including BlobUri
+                            refresh.Register("BlobSentinel", true);
                         })
                         .Map(async (setting) =>
                         {
                             if (setting.ContentType.Equals("application/storage.blob"))
                             {
-                                MyBlobContent? blobContent = await ReadBlobContentAsync(new Uri(setting.Value));
-                                string newSettingValue = "";
+                                string blobContent = await ReadBlobContentAsync(new Uri(setting.Value));
 
-                                if (blobContent != null)
-                                {
-                                    newSettingValue = $"[{string.Join(", ", blobContent.Data)}]";
-                                }
-                                else
-                                {
-                                    newSettingValue = "Error parsing Blob content.";
-                                }
-
-                                setting = new ConfigurationSetting(setting.Key, newSettingValue, setting.Label, setting.ETag);
+                                setting = new ConfigurationSetting(setting.Key, blobContent, setting.Label);
+                                setting.ContentType = "application/json";
                             }
 
                             return setting;
@@ -141,7 +126,9 @@ namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Examples.Cons
                 // Trigger and wait for an async refresh for registered configuration settings
                 await _refresher.TryRefreshAsync();
 
-                sb.AppendLine($"Blob content: {Configuration["BlobUri"]}");
+                IEnumerable<string?> blobData = Configuration.GetSection("BlobUri:Data").AsEnumerable().Where(kv => !string.IsNullOrEmpty(kv.Value)).Select(kv => $"{kv.Key} = {kv.Value}");
+
+                sb.AppendLine($"Blob content: \n{string.Join("\n", blobData)}");
                 sb.AppendLine();
 
                 sb.AppendLine("Press any key to exit...");
