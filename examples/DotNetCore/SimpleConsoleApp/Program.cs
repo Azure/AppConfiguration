7dac26a879ac74b77fee7f15ace9ec97e0182ebb
yuwe <yuwe@microsoft.com>
2023-07-17 11:15:11 +0800

    initialize demo project

---
+++ examples/DotNetCore/SimpleConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Microsoft.Extensions.Configuration.AzureAppConfiguration.Examples.SimpleConsoleApp
+{
+    using Microsoft.Extensions.Configuration;
+
+    class Program
+    {
+        static void Main(string[] args) {
+
+            var builder = new ConfigurationBuilder();
+            builder.AddAzureAppConfiguration(Environment.GetEnvironmentVariable("ConnectionString"));
+
+            var config = builder.Build();
+            Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+        }
+    }
+}
+
+
