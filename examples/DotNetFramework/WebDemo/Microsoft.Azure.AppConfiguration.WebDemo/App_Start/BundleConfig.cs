ed5efc1c517c614899166ea9e9ad270ca1ec9acb
AMER JUSUPOVIC <ajusupovic@microsoft.com>
2024-04-16 13:57:35 -0700

    WIP fix page not showing

--- examples/DotNetFramework/WebDemo/Microsoft.Azure.AppConfiguration.WebDemo/App_Start/BundleConfig.cs
+++ examples/DotNetFramework/WebDemo/Microsoft.Azure.AppConfiguration.WebDemo/App_Start/BundleConfig.cs
@@ -8,15 +8,15 @@ namespace Microsoft.Azure.AppConfiguration.WebDemo
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-3.7.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-3.12.0.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
