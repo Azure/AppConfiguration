f63b56f57b7d8a58c433feed0db5c298b6705cc8
Thomas Wu <t-thomaswu@microsoft.com>
2024-08-05 13:24:15 -0700

    Update examples/QuickStart/JavaSpring/QuickStart/src/main/java/app/config/QuickStart/MyProperties.java
    Co-authored-by: Matthew Metcalf <mrm9084@gmail.com>

--- examples/QuickStart/JavaSpring/QuickStart/src/main/java/app/config/QuickStart/MyProperties.java
+++ examples/QuickStart/JavaSpring/QuickStart/src/main/java/app/config/QuickStart/MyProperties.java
@@ -1,6 +1,8 @@
 package app.config.QuickStart;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+// The `prefix` value of `@ConfigurationProperties` matches the created key of `/application/config.message` so all properties with that prefix are added to this `@ConfigurationProperties`
+// Note: By default, `/application/` is removed from the key name.
 @ConfigurationProperties(prefix = "config")
 public class MyProperties {
     private String message;
