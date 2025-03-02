65278f7069a508ca149ee54a824a4e021ecef431
Zhenlan Wang <zhenlwa@microsoft.com>
2025-03-02 03:36:59 -0800

    Add Function App example with .NET 8 isolated model

---
+++ examples/DotNetCore/AzureFunction/FunctionAppIsolated/ShowBetaFeature.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+
+namespace FunctionAppIsolated
+{
+    public class ShowBetaFeature
+    {
+        private readonly IVariantFeatureManagerSnapshot _featureManager;
+        private readonly ILogger<ShowBetaFeature> _logger;
+
+        public ShowBetaFeature(IVariantFeatureManagerSnapshot featureManager, ILogger<ShowBetaFeature> logger)
+        {
+            _featureManager = featureManager;
+            _logger = logger;
+        }
+
+        [Function("ShowBetaFeature")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            // Read feature flag
+            string featureName = "Beta";
+            bool featureEnabled = await _featureManager.IsEnabledAsync(featureName);
+
+            return new OkObjectResult(featureEnabled
+                ? $"{featureName} feature is On"
+                : $"{featureName} feature is Off (or not found in Azure App Configuration).");
+
+        }
+    }
+}
