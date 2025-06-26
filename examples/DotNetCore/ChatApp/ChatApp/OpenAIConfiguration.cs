10a38d9095404a5ec56e9aac6a7fd5961e57e013
Zhenlan Wang <zhenlwa@microsoft.com>
2025-06-25 21:29:52 -0700

    Add readme

---
+++ examples/DotNetCore/ChatApp/ChatApp/OpenAIConfiguration.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+
+namespace ChatApp
+{
+    internal class OpenAIConfiguration
+    {
+        public required string Endpoint { get; set; }
+
+        public required string DeploymentName { get; set; }
+        
+        public string? ApiKey { get; set; }
+    }
+}
