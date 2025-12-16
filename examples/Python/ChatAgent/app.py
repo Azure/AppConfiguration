31cb4d84a5a93326120b361e8c6540e052abd5b6
Maryanne Gichohi <mgichohi@microsoft.com>
2025-12-16 20:46:45 +0300

    Update package version

--- examples/Python/ChatAgent/app.py
+++ examples/Python/ChatAgent/app.py
@@ -1,7 +1,6 @@
 import asyncio
 import os
 from agent_framework.declarative import AgentFactory
-from azure.identity.aio import DefaultAzureCredential as AsyncDefaultCredential
 from azure.identity import DefaultAzureCredential
 from azure.appconfiguration.provider import load
 
@@ -13,21 +12,20 @@ async def main():
 
     yaml_str = config["ChatAgent:Spec"]
     
-    async with (
-        AsyncDefaultCredential() as credential,
-        AgentFactory(client_kwargs={"async_credential": credential, "project_endpoint": config["ChatAgent:ProjectEndpoint"]}).create_agent_from_yaml(yaml_str) as agent,
-    ):
-        while True:
-            print("How can I help? (type 'quit' to exit)")
+    agent = AgentFactory(client_kwargs={"credential": credential, "project_endpoint": config["ChatAgent:ProjectEndpoint"]}).create_agent_from_yaml(yaml_str)
 
-            user_input = input("User: ")
+    while True:
+        print("How can I help? (type 'quit' to exit)")
+
+        user_input = input("User: ")
+        
+        if user_input.lower() in ['quit', 'exit', 'bye']:
+            break
+    
+        response = await agent.run(user_input)
+        print("Agent response: ", response.text)
+        input("Press enter to continue...")
             
-            if user_input.lower() in ['quit', 'exit', 'bye']:
-                break
-
-            response = await agent.run(user_input)
-            print("Agent response: ", response.text)
-            input("Press enter to continue...")
             
         print("Exiting... Goodbye...")
 
